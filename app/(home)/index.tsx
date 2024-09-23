bae33105e75bb21454ed89277b82556448279b53
Robson Diego Andrade <Robindiegoa@gmail.com>
2024-09-23 17:44:41 -0300

    :sparkles: feat: creating function to add and remove participant

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -4,16 +4,36 @@ import { colors } from "@/utils/theme";
 import { ParticipantCard } from "@/components/home/participant-card/participant-card";
 import { useState } from "react";
 import { Formik } from 'formik';
+import Toast from 'react-native-root-toast';
 import { addParticipantSchema, IAddParticipant, initialAddParticipantValue } from "@/schemas/addParticipant.schema";
 
 export default function Home() {
 
-  const [participants, setParticipants] = useState<string[] | null>(null)
+  const [participants, setParticipants] = useState<string[]>([])
   const todayDate = new Date()
   const formattedDate = todayDate.toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
   const handleAddParticipant = ({ name }: IAddParticipant) => {
+    const isNameAdded = participants.find(participant => participant === name)
+    if (isNameAdded) {
+      return Toast.show('Nome já adicionado a lista.', {
+        backgroundColor: "#d45454",
+      });
+    }
 
+    if (participants.length > 0) {
+      setParticipants([...participants, name])
+    } else {
+      setParticipants([name])
+    }
+
+  }
+
+  const handleRemoveParticipant = (index: number) => {
+    if (participants) {
+      const newParticipants = participants.filter((_, i) => i !== index)
+      setParticipants(newParticipants)
+    }
   }
 
   return (
@@ -28,7 +48,7 @@ export default function Home() {
         validationSchema={addParticipantSchema}
       >
         {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-          <>
+          <View>
             <View style={styles.insertNameInputContainer}>
               <TextInput
                 style={styles.insertNameInput}
@@ -48,18 +68,27 @@ export default function Home() {
                 </Text>
               </TouchableOpacity>
             </View>
-            {errors.name && touched.name && <Text style={{ color: "#fff" }}>{errors.name}</Text>}
-          </>
+            {errors.name && touched.name && <Text style={{ color: "#d45454" }}>{errors.name}</Text>}
+          </View>
         )}
       </Formik>
       <View style={styles.participantsContainer}>
         <Text style={styles.participantsTitle}>Participantes</Text>
-        <Text style={styles.participantsInitialText}>
-          Ninguém chegou no evento ainda? {"\n"} Adicione participantes a sua lista de presença
-        </Text>
-        <View style={styles.participantsCardContainer}>
-          <ParticipantCard />
-        </View>
+        {
+          participants.length > 0 ?
+            participants.map((name, index) => (
+              <ParticipantCard
+                key={index}
+                index={index}
+                participantName={name}
+                handleRemoveParticipant={handleRemoveParticipant}
+              />
+            ))
+            :
+            <Text style={styles.participantsInitialText}>
+              Ninguém chegou no evento ainda? {"\n"} Adicione participantes a sua lista de presença
+            </Text>
+        }
       </View>
     </View>
   );
--- components/home/participant-card/participant-card.tsx
+++ components/home/participant-card/participant-card.tsx
@@ -1,13 +1,21 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./style";
 
-export function ParticipantCard() {
+interface IParticipantCard {
+    index: number
+    participantName: string
+    handleRemoveParticipant: (index: number) => void
+}
+
+export function ParticipantCard({ participantName, handleRemoveParticipant, index }: IParticipantCard) {
     return (
         <View style={styles.participantCard}>
             <Text style={styles.participantName}>
-                Robson Diego
+                {participantName}
             </Text>
-            <TouchableOpacity style={styles.deleteParticipantButton}>
+            <TouchableOpacity
+                style={styles.deleteParticipantButton}
+                onPress={() => handleRemoveParticipant(index)}>
                 <Text style={styles.deleteParticipantButtonText}>
                     -
                 </Text>
